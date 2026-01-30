e3137320c4d633d4f5c7d8a4e32a3fcddcd6ed60
xCrucified <eexet17@gmail.com>
2026-01-30 03:05:11 +0100

    feat: update package dependencies and add new components
    - Updated package.json to include new dependencies: @radix-ui/react-slot, autoprefixer, class-variance-authority, clsx, lucide-react, postcss, tailwind-merge, and tailwindcss.
    - Removed unused SVG files: file.svg, globe.svg, next.svg, vercel.svg, and window.svg.
    - Added new Header component for site navigation.
    - Created a reusable Button component with variant support.
    - Introduced utility functions for class name management.
    - Added new SVG assets for header and icons.
    - Created components.json for component configuration.

--- vladizziitattoo/app/layout.tsx
+++ vladizziitattoo/app/layout.tsx
@@ -1,17 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
+import Header from "./components/shared/header";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const montserrat = Montserrat({
+  variable: "--font-montserrat",
   subsets: ["latin"],
 });
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,10 +19,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="font-thin">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${montserrat.variable} antialiased`}
       >
+        <Header />
         {children}
       </body>
     </html>
