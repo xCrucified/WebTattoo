48c6315f2cc372c5fbd02c7d99355a689fd0b393
xCrucified <eexet17@gmail.com>
2026-02-16 00:45:17 +0100

    upload background

--- vladizziitattoo/app/components/shared/main.tsx
+++ vladizziitattoo/app/components/shared/main.tsx
@@ -1,5 +1,5 @@
-import { cn } from '@/lib/utils';
-import React from 'react';
+import { cn } from "@/lib/utils";
+import React from "react";
 
 interface Props {
   className?: string;
@@ -7,9 +7,25 @@ interface Props {
 
 export const Main: React.FC<Props> = ({ className }) => {
   return (
-    <div className={cn(className, "mt-32 text-white text-2xl outline-1 h-[2200px] w-full")}>
-        main
-    </div>
+    <main
+  className={cn(
+    className,
+    "relative mt-32 text-white text-2xl w-full min-h-screen overflow-hidden"
+  )}
+>
+  <video
+    className="absolute top-0 left-0 object-cover z-10 w-full h-full"
+    src="/bg.mp4"
+    autoPlay
+    loop
+    muted
+    playsInline
+  />
+
+  <div className="content relative z-10">
+    <h1>Твой заголовок</h1>
+  </div>
+</main>
   );
 };
 
