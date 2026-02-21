f9444b6c93fdc41edb86d1cedba553b2bc4f69c2
xCrucified <eexet17@gmail.com>
2026-02-21 23:28:42 +0100

    add video background to main component and update styles

--- vladizziitattoo/app/components/shared/main.tsx
+++ vladizziitattoo/app/components/shared/main.tsx
@@ -8,24 +8,25 @@ interface Props {
 export const Main: React.FC<Props> = ({ className }) => {
   return (
     <main
-  className={cn(
-    className,
-    "relative mt-32 text-white text-2xl w-full min-h-screen overflow-hidden"
-  )}
->
-  <video
-    className="absolute top-0 left-0 object-cover z-100 w-full h-full"
-    src="/bg.mp4"
-    autoPlay
-    loop
-    muted
-    playsInline
-  />
+      className={cn(
+        className,
+        "relative mt-32 text-white text-2xl w-full min-h-screen overflow-hidden",
+      )}
+    >
+      <video
+        className="absolute top-0 left-0 object-cover w-full h-[100vh]"
+        autoPlay
+        loop
+        muted
+        playsInline
+      >
+        <source src="/bg.mp4" type="video/mp4" className="w-full h-full" />
+      </video>
 
-  <div className="content relative z-10">
-    <h1>Твой заголовок</h1>
-  </div>
-</main>
+      <div className="content relative z-10 w-full h-[100vh]">
+        <h1>Твой заголовок</h1>
+      </div>
+    </main>
   );
 };
 
