f0580983a2e9df608c5e4a06a0b7a8dda964ab1a
xCrucified <eexet17@gmail.com>
2026-03-24 20:09:59 +0100

    remove package.json and package-lock.json; update main component styles and video source; add new background videos

--- vladizziitattoo/app/components/shared/main.tsx
+++ vladizziitattoo/app/components/shared/main.tsx
@@ -10,17 +10,17 @@ export const Main: React.FC<Props> = ({ className }) => {
     <main
       className={cn(
         className,
-        "relative mt-32 text-white text-2xl w-full min-h-screen overflow-hidden",
+        "relative mt-32 bg-black text-white text-2xl w-full min-h-screen overflow-hidden",
       )}
     >
       <video
-        className="absolute top-0 left-0 object-cover w-full h-[100vh]"
+        className="absolute top-0 left-0 object-cover w-full h-[100vh] opacity-35"
         autoPlay
         loop
         muted
         playsInline
       >
-        <source src="/bg.mp4" type="video/mp4" className="w-full h-full" />
+        <source src="/bg3hevc.mp4" type="video/mp4" className="w-full h-full" />
       </video>
 
       <div className="content relative z-10 w-full h-[100vh]">
