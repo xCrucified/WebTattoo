d1ec469882c514baf20f93a57310378cbd241dcd
xCrucified <eexet17@gmail.com>
2026-07-04 02:32:55 +0200

    chore: update Prisma dependencies and add database schema for Gallery and GalleryImage models fixed.

--- vladizziitattoo/lib/prisma.ts
+++ vladizziitattoo/lib/prisma.ts
@@ -1,12 +1,8 @@
-import { PrismaClient } from '@prisma/client';
 
+import { PrismaClient } from '@prisma/client';
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ['query', 'error', 'warn'], // Это поможет увидеть реальную ошибку в терминале
-  });
+export const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') {
   globalForPrisma.prisma = prisma;
